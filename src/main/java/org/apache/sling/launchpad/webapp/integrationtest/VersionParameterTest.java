3b510c8cb0ddf295b550c878317c53be2dc9b178
Matthias Herold <alldaysunshine@icloud.com>
2020-05-05 08:38:31 +0200

    SLING-9417 Added test for an nt:file child of a versioned node

--- src/main/java/org/apache/sling/launchpad/webapp/integrationtest/VersionParameterTest.java
+++ src/main/java/org/apache/sling/launchpad/webapp/integrationtest/VersionParameterTest.java
@@ -16,6 +16,8 @@
  */
 package org.apache.sling.launchpad.webapp.integrationtest;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -72,6 +74,70 @@ public class VersionParameterTest extends HttpTestBase {
         assertEquals("v2", getProp(location + ";v='1.1'.json"));
     }
 
+    public void testVersionParameterWithChildNode() throws IOException {
+        Map<String, String> versionableNodeParams = new HashMap<>();
+        versionableNodeParams.put("jcr:mixinTypes", "mix:versionable");
+
+        final String testFileContent = "test-file-content";
+        final String testFileContentUpdated = "test-file-content-v2";
+
+        /* 
+         * Step 1: Create a node
+         */
+        params.put(":checkinNewVersionableNodes", "false");
+        params.put("prop", "parent");
+
+        final String versionableNodeLocation = testClient.createNode(postUrl + SlingPostConstants.DEFAULT_CREATE_SUFFIX, params);
+        final String childFileNodeLocation = versionableNodeLocation + "/childFile";
+
+        /*
+         * Step 2: Upload a file as a child of the node
+         */
+        InputStream inputStream = new ByteArrayInputStream(testFileContent.getBytes());
+        testClient.upload(childFileNodeLocation, inputStream);
+
+        /*
+         * Validation: Check that the uploaded content can be received
+         */
+        String content = getContent(childFileNodeLocation, CONTENT_TYPE_DONTCARE);
+        assertEquals(testFileContent, content);
+
+        /*
+         * Step 3: Create a new version of the node
+         */
+        Map<String, String> versionableNodeUpdatedParams = new HashMap<>();
+        versionableNodeUpdatedParams.put(":operation", "checkin");
+        testClient.post(versionableNodeLocation, versionableNodeUpdatedParams);
+
+        versionableNodeUpdatedParams.put(":operation", "checkout");
+        testClient.post(versionableNodeLocation, versionableNodeUpdatedParams);
+
+        /* 
+         * Step 4: Update the child node content
+         */
+        InputStream updatedInputStream = new ByteArrayInputStream(testFileContentUpdated.getBytes());
+        testClient.upload(childFileNodeLocation, updatedInputStream);
+
+        /*
+         * Validation: Check that the current content can be received
+         */
+        String updatedContent = getContent(childFileNodeLocation, CONTENT_TYPE_DONTCARE);
+        assertEquals(testFileContentUpdated, updatedContent);
+
+        /*
+         * Validation: Try to receive original version of the node (version 1.0)
+         */
+         assertEquals("parent", getProp(versionableNodeLocation + ".json;v='1.0'"));
+       
+        /*
+         * Validation: Try to receive previous content of the child  (version 1.0)
+         */
+
+        String previousContent = getContent(childFileNodeLocation + ";v='1.0'", CONTENT_TYPE_DONTCARE);
+        assertEquals(testFileContent, previousContent);
+        
+    }
+
     private void waitUntilSlingIsStable() throws HttpException, IOException,
             InterruptedException {
         for (int i = 0; i < 10; i++) {
@@ -82,7 +148,7 @@ public class VersionParameterTest extends HttpTestBase {
         }
         fail("Sling instance fails to respond with status 200");
     }
-    
+
     private String getProp(String url) throws JsonException, IOException {
         final JsonObject content = JsonUtil.parseObject(getContent(url, CONTENT_TYPE_JSON));
         return content.getString("prop");
